af777c5e2e7cedce1f508c38f27da4552a5ecc74
Gabriel Reira <gabrielmfreira@gmail.com>
2023-08-25 20:26:00 -0300

    acrescentando novas funcionalidades

--- 01 - Temperature Time Series For Cities/app.py
+++ 01 - Temperature Time Series For Cities/app.py
@@ -1,9 +1,11 @@
 from dash import Dash, html, dcc, callback, Output, Input
 import plotly.express as px
+import plotly.graph_objs as go
 import data_treat
 
+
 # Importando os dados
-df = data_treat.data
+df = data_treat.load_data()
 df_cities = data_treat.get_cities(df)
 df_options = data_treat.get_options(df)
 
@@ -12,24 +14,57 @@ app = Dash(__name__)
 
 app.layout = html.Div([
   html.Div(className='row', children=[
-  html.H1(children='Temperatura de Cidades Brasileiras'),
+    html.H1(children='Temperatura de Cidades Brasileiras'),
 
-  dcc.RadioItems(options=df_cities,value="salvador", inline=True, id='city-radio')]),
+    dcc.Checklist(options=df_cities,value=["salvador"], inline=True, id='city-checklist')]),
+    html.Div(className='row', children=[
+      dcc.Dropdown(options=df_options, value="JAN",id='month-dropdown')
+    ]),
+    dcc.Input(id='outlier-multiplier', type='number', value=1.5),
+    dcc.RadioItems(
+      options=[
+        {'label': 'nada', 'value': 'nada'},
+        {'label': 'último valor válido', 'value': 'fillna'},
+        {'label': 'interpolacao linear', 'value': 'interpolacao'}
+      ], 
+      value="nada",
+      id='method-radio'
+    ),
 
-  html.Div(className='row', children=[
-    dcc.Dropdown(options=df_options, value="JAN",id='month-radio')
-  ]),
-  dcc.Graph(figure=px.line(df["salvador"], x='YEAR', y='JAN'), id='graph')
+    dcc.Graph(figure=px.line(df["salvador"], x='YEAR', y='JAN'), id='graph'),
+    dcc.Graph(figure=px.line(df["salvador"], x='YEAR', y='JAN'), id='box-plot')
 ])
 
+# Gráfico de linhas
 @callback(
-  Output(component_id='graph', component_property='figure'),
-  Input(component_id='city-radio', component_property='value'),
-  Input(component_id='month-radio', component_property='value')
+  Output('graph', 'figure'),
+  Input('city-checklist', 'value'),
+  Input('month-dropdown', 'value'),
+  Input('outlier-multiplier', 'value'),
+  Input('method-radio', 'value')
 )
+def update_graph(cities_chosen, month_chosen, multiplier, filling_method):
+  data = data_treat.load_data()
+  data1 = data_treat.set_outlier_multiplier(data, multiplier)
+  data1 = data_treat.set_fill(data, filling_method)
 
-def update_graph(city_chosen, month_chosen):
-  fig = px.line(df[city_chosen], x='YEAR', y=month_chosen)
+  fig = px.line()
+  for city in cities_chosen:
+    fig.add_trace(go.Scatter(x=data1[city]['YEAR'], y=data1[city][month_chosen], name=city))
+
+  return fig
+
+# Gráfico Box Plot
+@callback(
+  Output('box-plot', 'figure'),
+  Input('city-checklist', 'value')
+)
+def update_box_plot(cities_chosen):
+  data = []
+  for city in cities_chosen:
+    data.append(go.Box(y=df[city][df[city].columns[1:]], name=city))  
+  fig = go.Figure(data)
+
   return fig
 
 if __name__ == '__main__':
--- 01 - Temperature Time Series For Cities/data_treat.py
+++ 01 - Temperature Time Series For Cities/data_treat.py
@@ -1,26 +1,34 @@
 import pathlib
 import pandas as pd
 import os
+import numpy as np
+
 
 # Agrupando os dados num dict
-data_dir = pathlib.Path('data')
-data = {}
-for file_name in os.listdir(data_dir):
-  city_name = file_name[8:].split('.')[0]
-  data[city_name] = pd.read_csv(os.path.join(data_dir,file_name))
+def load_data():
+  data_dir = pathlib.Path('data')
+  data = {}
+  for file_name in os.listdir(data_dir):
+    city_name = file_name[8:].split('.')[0]
+    data[city_name] = pd.read_csv(os.path.join(data_dir,file_name))
 
+  # Eliminando colunas
+  for k in data.keys():
+    data[k].drop(['D-J-F','M-A-M','J-J-A','S-O-N'], axis=1, inplace=True)
 
-# Eliminando colunas
-for k in data.keys():
-  data[k].drop(['D-J-F','M-A-M','J-J-A','S-O-N'], axis=1, inplace=True)
+  # Eliminar valores 999
+  for k in data.keys():
+    data[k].replace(999.90, np.NaN, inplace=True)
+  
+  return data
 
 
 # Eliminando outliers
-def get_limits(column):
+def get_limits(column, multiplier):
   q1 = column.quantile(0.25) # corresponde à 25% dos dados
   q3 = column.quantile(0.75) # corresponde ao restante (75%)
   iqr = q3 - q1
-  limits = q1 - 1.5 * iqr, q3 + 1.5 * iqr
+  limits = q1 - multiplier * iqr, q3 + multiplier * iqr
   return limits
 
 def delete_outliers_per_column(df, column_name):
@@ -37,8 +45,13 @@ def delete_outliers(df):
     #print(f'{deleted_rows} linha(s) removida(s).')
   return df
 
-for k in data.keys():
-  data[k] = delete_outliers(data[k])
+# Preenchendo outliers com NaN
+def filling_outliers(df, multiplier):
+  for month in df.columns[1:13]:
+    lower_lim, upper_lim = get_limits(df[month], multiplier)
+    outlier_indices = df[(df[month] > upper_lim) | (df[month] < lower_lim)].index
+    df.loc[outlier_indices, month] = np.nan
+  return df
 
 
 # Funções de interesse para o app
@@ -56,4 +69,21 @@ def get_options(data):
     item["value"] = month
     options.append(item)
   return options
-  
+
+def set_outlier_multiplier(df, multiplier):
+  for k in df.keys():
+    df[k] = filling_outliers(df[k], multiplier)
+  return df
+
+def set_fill(df, tipo):
+  if(tipo == "nada"):
+    return df
+  elif(tipo == "fillna"):
+    for k in df.keys():
+      df[k] = df[k].fillna(method="ffill")
+    return df
+  elif(tipo == "interpolacao"):
+    for k in df.keys():
+      df[k] = pd.DataFrame(df[k])
+      df[k] = df[k].interpolate(method='linear')
+    return df
